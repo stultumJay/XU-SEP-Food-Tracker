4ec2c0d43feda82fcc809f4ac3aecdfd73de0fcc
Gerfel Jay <144815217+stultumJay@users.noreply.github.com>
2025-06-25 22:13:06 +0800

    Add AppContextListener: Initialize DBUtil at application startup using ServletContextListener.

---
+++ src/main/java/SecHallSolution/util/AppContextListener.java
@@ -0,0 +1,13 @@
+package SecHallSolution.util;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        DBUtil.init(sce.getServletContext());
+    }
+} 
